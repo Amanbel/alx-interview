94aa5dfa71b4ee7bffdfe80cb290ca0fb97e5aaa
Amanbel <amanuel11belachew@gmail>
2024-05-09 22:36:08 +0300

    width-fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,8 +9,17 @@ def island_perimeter(grid):
     function that returns the perimeter
     of the grid
     """
-    height = len(grid)
+    height = 0
+    width = 0
+    max = 0
     for h in grid:
-        width = len(h)
-        break
-    return height + width
+        if 1 in h:
+            height += 1
+    for h in grid:
+        width = 0
+        for w in h:
+            if w == 1:
+                width += 1
+        if width > max:
+            max = width
+    return 2 * (max + height)
