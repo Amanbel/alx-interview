aed55c91135a6fcbd5babaa8042c0c8f9c62987f
Amanbel <amanuel11belachew@gmail>
2024-05-16 23:16:28 +0300

    reverted

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,34 +10,21 @@ def isWinner(x, nums):
     """
     maria = 2
     mariasSet = []
-    mariasPick = []
     ben = 3
     bensSet = []
-    bensPick = []
     while x > 0:
         x -= 1
         for n in nums:
-            while len(mariasPick + bensPick) + 1 != n:
-                for i in range(1, n + 1):
-                    if i in bensPick:
-                        continue
-                    if maria % i == 0:
-                        mariasSet.append(i)
-                        mariasPick.append(i)
-                for i in range(1, n + 1):
-                    if i in mariasPick:
-                        continue
-                    if ben % i == 0:
-                        bensSet.append(i)
-                        bensPick.append(i)
-                if n == 1:
-                    bensSet.append(1)
-                maria += 1
-                ben += 1
-            mariasPick = []
-            bensPick = []
-            maria = 2
-            ben = 3
+            for i in range(1, n + 1):
+                if maria % i == 0:
+                    mariasSet.append(i)
+            for i in range(1, n + 1):
+                if ben % i == 0:
+                    bensSet.append(i)
+            if n == 1:
+                bensSet.append(1)
+            maria += 1
+            ben += 1
     if len(bensSet) > len(mariasSet):
         return "Ben"
     elif len(bensSet) == len(mariasSet):
