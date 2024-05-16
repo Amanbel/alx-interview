94e8e8b7b441a032b6ac04abf15a85fce45f145a
Amanbel <amanuel11belachew@gmail>
2024-05-16 13:39:55 +0300

    fix

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -10,15 +10,20 @@ def isWinner(x, nums):
     """
     maria = 2
     mariasSet = []
+    mariasPick = []
     ben = 3
     bensSet = []
+    bensPick = []
     while x > 0:
         x -= 1
         for n in nums:
             for i in range(1, n + 1):
                 if maria % i == 0:
                     mariasSet.append(i)
+                    mariasPick.append(i)
             for i in range(1, n + 1):
+                if ben in mariasPick:
+                    ben += 1
                 if ben % i == 0:
                     bensSet.append(i)
             if n == 1:
